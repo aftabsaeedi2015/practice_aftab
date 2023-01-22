d8147d69ed40e212e45ec06cfdb6d2f06153ae59
aftab <aftab.saeedi2015@gmail.com>
2023-01-22 19:10:36 +0530

    added multiply function to java file

--- Calculator.java
+++ Calculator.java
@@ -5,4 +5,7 @@ public class Calculator {
     public void subtract(int a , int b ){
         System.out.println(a-b);
     }
+    public void multiply(int a , int b ){
+        System.out.println(a*b);
+    }
 }
